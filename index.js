75a5318f512b2cb8231986e29178736bac47d5a4
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-10-14 18:16:08 +0900

    add proxy middleware

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const url = require("url");
 
 app.get("/", (req, res) => {
 	res.send("This is my proxy server")
-})
+});
+
+app.use("/world-weather", (req, res, next) => {
+	const city = req.query.city
+	createProxyMiddleware({
+		target: `https://api.weatherapi.com/v1/current.json?key=2dc5159ca1f2424599874900210112&q=${city}&aqi=no`,
+		changeOrigin: true,
+		// pathRewrite: {
+		// 	[]: "",
+		// },
+	})(req, res, next);
+});
 
 app.listen(5000, () => {
 	console.log("Listening on local port 5000");
