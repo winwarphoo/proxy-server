4ebd9985f9455abd2aebcea3018579be41c98170
winwarphoo <winwarphoo@mm.cybermissions.co.jp>
2022-10-14 18:26:36 +0900

    rate-limit

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const { createProxyMiddleware } = require("http-proxy-middleware");
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 100,
+})
 const url = require("url");
 
 app.get("/", (req, res) => {
 	res.send("This is my proxy server")
 });
 
-app.use("/world-weather", (req, res, next) => {
+app.use("/world-weather", limiter, (req, res, next) => {
 	const city = req.query.city
 	createProxyMiddleware({
 		target: `https://api.weatherapi.com/v1/current.json?key=2dc5159ca1f2424599874900210112&q=${city}&aqi=no`,
